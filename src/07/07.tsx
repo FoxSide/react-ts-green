e4964644a9f2bca821ffccf82450a594d1bda851
FoxSide <foxside8452@gmail.com>
2021-11-19 16:53:30 +0200

    07-ln

---
+++ src/07/07.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+type ManType = {
+  name: string
+  age: number
+  lessons: Array<{ tittle: string }>
+  adress: {
+    street: {
+      title: string
+    }
+  }
+}
+
+type PropsType = {
+  title: string
+  man: ManType
+  food: Array<string>
+  car: {
+    model: string
+  }
+}
+
+// function useState(m: string) {
+//   return [m, function (){}]
+// }
+
+export const ManComponent: React.FC<PropsType> = ({title, man, ...props}) => {
+  // const [message, setMessage] = useState('hello')
+  return <div>
+  <h1>{title}</h1>
+    <hr/>
+    <div>
+      {props.car.model}
+    </div>
+    <div>
+      {man.name}
+    </div>
+  </div>
+}
